8acfa850bfc1cfbbd21f6cf5214c8a0debf5ee94
Vivianyu116 <42662137+Vivianyu116@users.noreply.github.com>
2018-08-26 20:35:52 +0800

    Add files via upload

---
+++ guessnum.py
@@ -0,0 +1,20 @@
+import random
+
+r = random.randint(1, 100)
+print(r)
+
+while True:
+	num=input('請猜一數字:')
+	num=int(num)
+	
+	if num == r:
+		print('你猜中了')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
+
+
+
+
